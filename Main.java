14bcd6195e1f4a58dbef04ef6566f9c14089e6da
TaimoorPal <67574214+TaimoorPal@users.noreply.github.com>
2022-12-07 11:40:42 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,37 +1,38 @@
-package Lab7;
+package Lab7_Main;
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-import RobotClassAssignment.POM;
+import org.testng.annotations.Test;
 
 public class Main {
-
 	WebDriver driver;
-	Logger log = Logger.getLogger("MainProgram");
+	//Logger log = Logger.getLogger("MainProgram");
 	POM obj;
 	Actions act;
 
 	@BeforeMethod
 	public void Setup() {
 		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\4397\\Documents\\SQA\\chromedriver_win32\\chromedriver.exe");
+				"C:\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject");
-		PropertyConfigurator.configure("log4j.properties");
+		//PropertyConfigurator.configure("log4j.properties");
 
 	}
 
-	@AfterMethod
-	public void close() {
-		driver.close();
-	}
-	public void Test1() {
-		
+	//@AfterMethod
+	//public void close() {
+	//	driver.close();
+	//}
+	@Test
+	public void Test1() throws InterruptedException {
+		obj = new POM(driver);
+		obj.ManagerLogin();
+		obj.AddCustomer();
+		obj.AddCustomerAccount("Taimoor", "Ahmed", "52000");
+		obj.OpenAccount();
 	}
 }
